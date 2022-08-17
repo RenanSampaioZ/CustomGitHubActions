6de42fb00ba0a93cb1a3aca067943dcd028cd918
unknown <renansampaio04@yahoo.com.br>
2022-08-17 16:49:17 -0300

    Tests

--- index.js
+++ index.js
@@ -8,27 +8,30 @@ const repo = core.getInput('repo', { required: true });
 const owner = core.getInput('owner', { required: true });
 const octokit = new github.getOctokit(token);
   
-const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+const last_tag_name = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
     owner: owner,
     repo: repo
   })
 
+const array_last_tag_name = last_tag_name.split('')
 
-console.log(parseFloat(last_tag.data.tag_name))
+array_last_tag_name.shift()
+
+const last_tag_number = array_last_tag_name.join('')
 
 // console.log(String(parseFloat(last_tag.data.tag_name) + parseFloat("0.1")))
 
-// await octokit.request('POST /repos/{owner}/{repo}/releases', {
-//   owner: owner,
-//   repo: repo,
-//   tag_name: String(parseFloat(last_tag.data.tag_name) + parseFloat("0.1")),
-//   target_commitish: 'master',
-//   name: String(parseFloat(last_tag.data.tag_name) + parseFloat("0.1")),
-//   body: 'Description of the release',
-//   draft: false,
-//   prerelease: false,
-//   generate_release_notes: false
-// })
+await octokit.request('POST /repos/{owner}/{repo}/releases', {
+  owner: owner,
+  repo: repo,
+  tag_name: String(parseFloat(last_tag_number) + parseFloat("0.1")),
+  target_commitish: 'master',
+  name: String(parseFloat(last_tag_number) + parseFloat("0.1")),
+  body: 'Description of the release',
+  draft: false,
+  prerelease: false,
+  generate_release_notes: false
+})
 }
 
 // Call the main function to run the action
