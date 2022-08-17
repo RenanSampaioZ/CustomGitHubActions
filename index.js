59ffcbcbaebca9914c492ab63335bad68d3fce89
unknown <renansampaio04@yahoo.com.br>
2022-08-17 15:56:24 -0300

    Tests

--- index.js
+++ index.js
@@ -13,20 +13,21 @@ const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/lates
     repo: repo
   })
 
-console.log(last_tag.data.tag_name + '0.1.0')
 
-// await octokit.request('POST /repos/{owner}/{repo}/releases', {
-//   owner: owner,
-//   repo: repo,
-//   tag_name: last_tag + '0.1.0',
-//   target_commitish: 'master',
-//   name: last_tag + '0.1.0',
-//   body: 'Description of the release',
-//   draft: false,
-//   prerelease: false,
-//   generate_release_notes: false
-// })
+
+await octokit.request('POST /repos/{owner}/{repo}/releases', {
+  owner: owner,
+  repo: repo,
+  tag_name: toString(parseFloat(last_tag.data.tag_name) + parseFloat("0.1")),
+  target_commitish: 'master',
+  name: toString(parseFloat(last_tag.data.tag_name) + parseFloat("0.1")),
+  body: 'Description of the release',
+  draft: false,
+  prerelease: false,
+  generate_release_notes: false
+})
 }
 
 // Call the main function to run the action
+
 main();
