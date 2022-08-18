a47590ca8838a5a5f8e0de80c2fb45491e1bd353
unknown <renansampaio04@yahoo.com.br>
2022-08-18 11:14:08 -0300

    tests

--- index.js
+++ index.js
@@ -14,32 +14,45 @@ const commits = await octokit.request('GET /repos/{owner}/{repo}/compare/{basehe
   basehead: 'develop...hmg'
 })
 
+const pull_request = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+  owner: 'OWNER',
+  repo: 'REPO'
+})
+
 console.log(commits)
   
-// const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
-//     owner: owner,
-//     repo: repo
-//   })
+const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+    owner: owner,
+    repo: repo
+  })
 
-// const last_tag_name = last_tag.data.tag_name
+const last_tag_name = last_tag.data.tag_name
 
-// const array_last_tag_name = last_tag_name.split('')
+const array_last_tag_name = last_tag_name.split('')
 
-// array_last_tag_name.shift()
+array_last_tag_name.shift()
 
-// const last_tag_number = array_last_tag_name.join('')
+const last_tag_number = array_last_tag_name.join('')
 
-// await octokit.request('POST /repos/{owner}/{repo}/releases', {
-//   owner: owner,
-//   repo: repo,
-//   tag_name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
-//   target_commitish: 'hmg',
-//   name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
-//   body: 'Description of the release',
-//   draft: false,
-//   prerelease: false,
-//   generate_release_notes: false
-// })
+await octokit.request('POST /repos/{owner}/{repo}/releases', {
+  owner: owner,
+  repo: repo,
+  tag_name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
+  target_commitish: 'hmg',
+  name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
+  body: 'Description of the release',
+  draft: false,
+  prerelease: false,
+  generate_release_notes: false
+})
+
+await octokit.request('POST /repos/{owner}/{repo}/releases/generate-notes', {
+  owner: owner,
+  repo: repo,
+  tag_name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
+  target_commitish: 'hmg',
+  previous_tag_name: last_tag_number,
+})
 }
 
 
