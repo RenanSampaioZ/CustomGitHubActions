d2a8141d71a0e2a0e581fed064f89b260799b53d
unknown <renansampaio04@yahoo.com.br>
2022-08-18 10:27:20 -0300

    tests

--- index.js
+++ index.js
@@ -7,31 +7,39 @@ const token = core.getInput('token', { required: true });
 const repo = core.getInput('repo', { required: true });    
 const owner = core.getInput('owner', { required: true });
 const octokit = new github.getOctokit(token);
-  
-const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
-    owner: owner,
-    repo: repo
-  })
 
-const last_tag_name = last_tag.data.tag_name
-
-const array_last_tag_name = last_tag_name.split('')
-
-array_last_tag_name.shift()
-
-const last_tag_number = array_last_tag_name.join('')
-
-await octokit.request('POST /repos/{owner}/{repo}/releases', {
-  owner: owner,
-  repo: repo,
-  tag_name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
-  target_commitish: 'hmg',
-  name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
-  body: 'Description of the release',
-  draft: false,
-  prerelease: false,
-  generate_release_notes: false
+const commits = await octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}', {
+  owner: 'OWNER',
+  repo: 'REPO',
+  basehead: 'hmg...develop'
 })
+
+console.log(commits)
+  
+// const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+//     owner: owner,
+//     repo: repo
+//   })
+
+// const last_tag_name = last_tag.data.tag_name
+
+// const array_last_tag_name = last_tag_name.split('')
+
+// array_last_tag_name.shift()
+
+// const last_tag_number = array_last_tag_name.join('')
+
+// await octokit.request('POST /repos/{owner}/{repo}/releases', {
+//   owner: owner,
+//   repo: repo,
+//   tag_name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
+//   target_commitish: 'hmg',
+//   name: 'v'+ String(parseFloat(last_tag_number) + parseFloat("0.1")),
+//   body: 'Description of the release',
+//   draft: false,
+//   prerelease: false,
+//   generate_release_notes: false
+// })
 }
 
 
