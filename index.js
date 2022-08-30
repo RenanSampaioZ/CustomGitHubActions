120a2443b65fb23250703d7951ccd5fac86817fd
RenanSampaio04 <renansampaio0401@yahoo.com>
2022-08-30 09:59:34 -0300

    testes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import libsodium from "libsodium-wrappers"
-import core from "@actions/core"
-import github from "@actions/github"
-
+const core = require('@actions/core');
+const github = require('@actions/github');
 const token = core.getInput('token', { required: true });
 const repo = core.getInput('repo', { required: true });    
 const owner = core.getInput('owner', { required: true });
@@ -9,37 +7,43 @@ const octokit = new github.getOctokit(token);
 
 const main = async () => {
 
+  try {
+    const last_tag = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+      owner: owner,
+      repo: repo
+    })
 
-  const key = await octokit.request('GET /repos/{owner}/{repo}/actions/secrets/public-key', {
-    owner: owner,
-    repo: repo
-  })
+    const last_tag_name = last_tag.data.tag_name
 
+    const array_last_tag_name = last_tag_name.split('')
 
-  const secretValue = core.getInput('secretValue', { required: true });    
-  const secretName = core.getInput('secretName', { required: true });
+    array_last_tag_name.shift()
 
-  // Convert the message and key to Uint8Array's (Buffer implements that interface)
-  const messageBytes = Buffer.from(secretValue);
-  const keyBytes = Buffer.from(key.data.key, 'base64');
+    const last_tag_number = array_last_tag_name.join('')
 
-  // Encrypt using LibSodium.
-  const encryptedBytes = libsodium.crypto_box_seal(messageBytes, keyBytes);
+    await octokit.request('POST /repos/{owner}/{repo}/releases', {
+      owner: owner,
+      repo: repo,
+      tag_name: 'v'+ (parseFloat(last_tag_number) + parseFloat("0.1")).toFixed(1),
+      target_commitish: 'hmg',
+      name: 'v'+ (parseFloat(last_tag_number) + parseFloat("0.1")).toFixed(1),
+      draft: false,
+      prerelease: false,
+      generate_release_notes: true
 
-  // Base64 the encrypted secret
-  const encrypted = Buffer.from(encryptedBytes).toString('base64');
-
-  console.log(encrypted);
-
-
-  await octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
-    owner: owner,
-    repo: repo,
-    secret_name: secretName,
-    encrypted_value: encrypted,
-    key_id: key.data.key_id
-  })
-  
+    })       
+  } catch (error) {
+    await octokit.request('POST /repos/{owner}/{repo}/releases', {
+      owner: owner,
+      repo: repo,
+      tag_name: 'v1.0',
+      target_commitish: 'hmg',
+      name: 'v1.0',
+      draft: false,
+      prerelease: false,
+      generate_release_notes: false   
+    })     
+  }
 }
 
 // Call the main function to run the action
---
+++ index_create-secret.js
@@ -0,0 +1,45 @@
+import libsodium from "libsodium-wrappers"
+import core from "@actions/core"
+import github from "@actions/github"
+
+const token = core.getInput('token', { required: true });
+const repo = core.getInput('repo', { required: true });    
+const owner = core.getInput('owner', { required: true });
+const octokit = new github.getOctokit(token);
+
+const main = async () => {
+
+
+  const key = await octokit.request('GET /repos/{owner}/{repo}/actions/secrets/public-key', {
+    owner: owner,
+    repo: repo
+  })
+
+
+  const secretValue = core.getInput('secretValue', { required: true });    
+  const secretName = core.getInput('secretName', { required: true });
+
+  // Convert the message and key to Uint8Array's (Buffer implements that interface)
+  const messageBytes = Buffer.from(secretValue);
+  const keyBytes = Buffer.from(key.data.key, 'base64');
+
+  // Encrypt using LibSodium.
+  const encryptedBytes = libsodium.crypto_box_seal(messageBytes, keyBytes);
+
+  // Base64 the encrypted secret
+  const encrypted = Buffer.from(encryptedBytes).toString('base64');
+
+  await octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
+    owner: owner,
+    repo: repo,
+    secret_name: secretName,
+    encrypted_value: encrypted,
+    key_id: key.data.key_id
+  })
+  
+}
+
+// Call the main function to run the action
+
+
+main();
